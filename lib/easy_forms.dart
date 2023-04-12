0ab887f9cb8c545a99c12b47cf96a9b89015cc2a
Jaroslaw <jaroslaw@butajlo.dev>
2023-04-12 21:39:13 +0200

    rename files

--- lib/easy_forms.dart
+++ lib/easy_forms.dart
@@ -1,5 +1,5 @@
-export 'src/field_controller.dart';
-export 'src/form_controller.dart';
+export 'src/field.dart';
+export 'src/form.dart';
 export 'src/validation_node.dart';
 export 'src/extensions/submit_form_mixin.dart';
-export 'src/extensions/required_nullable_field.dart';
+export 'src/fields/required_nullable.dart';
